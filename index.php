982034c1a3e405e8c9a7509b1cf9a7155f935711
maxximall <maksimskaracuns@gmail.com>
2017-03-29 20:10:44 +0800

    first website upload

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Founders and Coders website</title>
+        <link rel="stylesheet" type="text/css" href="assets/styles.css">
+        <link rel="stylesheet" type="text/css" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    </head>
+    <body>
+
+        <section id="intro">
+
+            <div class="container">
+                <div class="row center">
+                    <div class="col col-1">
+                        <img src="assets/img/traveler.png" id="traveler">
+                    </div>
+                    <div class="col col-2">
+                        <h1>
+                            Hey, my name is Max ;&#41; <br>
+                            I am a uni studnet, backpacker and aspiring web developer.
+                        </h1>
+                    </div>
+                </div>
+            </div> <!-- container -->
+
+
+        </section><!-- intro -->
+
+
+
+        <section class="blue">
+            <svg id="clouds" class="cloud-invert" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h2>Have a look at my video application</h2>
+                        <div class="videoWrapper">
+                            <iframe width="853" height="480" src="https://www.youtube.com/embed/gSeF7KUva5Q?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                        </div>
+
+                    </div><!-- col -->
+
+                </div>
+            </div><!-- container -->
+
+        </section><!-- video -->
+
+        <section id="kata">
+            <svg id="clouds" class="cloud" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row center">
+                    <div class="col col-3 ">
+                        <h2>I just <i class="ion-heart"></i> to build stuff!<br/>
+                            Checkout my most recent project <a href="http://brainfriendz.com/" target="_blank" >BrainFriendz.com</a>
+                        </h2>
+                    </div>
+                    <div class="col col-3">
+                        <img src="assets/img/builder.png" id="builder">
+                    </div>
+                </div>
+            </div><!-- container -->
+        </section><!-- kata -->
+
+        <section id="project" class="blue">
+            <svg id="clouds" class="cloud-invert" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row center">
+                    <div class="col col-3">
+                        <img src="https://www.codewars.com/users/karacun/badges/large">
+                    </div>
+                    <div class="col col-3 align-left">
+                        <h3>
+                            Oh it was frustrating at times,
+                            but I made it!
+                        </h3>
+                    </div>
+                </div>
+            </div> <!-- container -->
+        </section><!-- project -->
+
+        <section id="sites">
+            <svg id="clouds" class="cloud" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row">
+                    <h2>Examples of the sites I've built</h2>
+                    <div class="site-feature-box">
+                        <a href="http://brainfriendz.com/" target="_blank">
+                            <div class="site-feature" id="first-site">
+
+                                <h3>BrainFriendz</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                    <div class="site-feature-box">
+                        <a href="http://artofweddings.lv/" target="_blank">
+                            <div class="site-feature" id="second-site">
+
+                                <h3>Art of Weddings</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                    <div class="site-feature-box">
+                        <a href="http://decorlessons.ru/promo/" target="_blank">
+                            <div class="site-feature" id="third-site">
+
+                                <h3>Decor Lessons</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                </div><!-- row -->
+
+                <div class="row">
+                    <div class="site-feature-box">
+                        <a href="http://wedmotions.com/" target="_blank">
+                            <div class="site-feature" id="fourth-site">
+
+                                <h3>Wedmotions</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                    <div class="site-feature-box">
+                        <a href="http://www.systemicaccountingservices.co.uk/tax-return/" target="_blank">
+                            <div class="site-feature" id="fifth-site">
+
+                                <h3>SAS</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                    <div class="site-feature-box">
+                        <a href="http://www.systemicaccountingservices.co.uk/tax-return/form.php" target="_blank">
+                            <div class="site-feature" id="sixth-site">
+
+                                <h3>SAS Form</h3>
+
+                            </div>
+                        </a> 
+                    </div>
+                    <p class="pssst">pssst... not super proud of all of them, but hey, I am still learing!</p>
+
+                </div><!-- row -->
+            </div><!-- container -->
+        </section><!-- sites -->
+
+        <section id="please" class="blue">
+            <svg id="clouds" class="cloud-invert" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row">
+                    <h2>Please, can I join you guys?</h2>
+                    <img src="assets/img/cat-eyes.png" id="cat-eyes">
+                    <h2>I promise, you will be proud of me!</h2>
+                </div>
+            </div><!-- container -->
+        </section><!-- sites -->
+
+        <footer>
+            <svg id="clouds" class="cloud" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none" >
+                <path d="M-5 100 Q 0 20 5 100 Z
+                         M0 100 Q 5 0 10 100
+                         M5 100 Q 10 30 15 100
+                         M10 100 Q 15 10 20 100
+                         M15 100 Q 20 30 25 100
+                         M20 100 Q 25 -10 30 100
+                         M25 100 Q 30 10 35 100
+                         M30 100 Q 35 30 40 100
+                         M35 100 Q 40 10 45 100
+                         M40 100 Q 45 50 50 100
+                         M45 100 Q 50 20 55 100
+                         M50 100 Q 55 40 60 100
+                         M55 100 Q 60 60 65 100
+                         M60 100 Q 65 50 70 100
+                         M65 100 Q 70 20 75 100
+                         M70 100 Q 75 45 80 100
+                         M75 100 Q 80 30 85 100
+                         M80 100 Q 85 20 90 100
+                         M85 100 Q 90 50 95 100
+                         M90 100 Q 95 25 100 100
+                         M95 100 Q 100 15 105 100 Z"></path>
+            </svg>
+            <div class="container">
+                <div class="row">
+                    <h2>Wanna say hello? Reach me via <a href="https://www.facebook.com/maksim.karacun" target="_blank">Facebook</a>, <a href="https://www.instagram.com/karacun/" target="_blank">Instagram</a> or <a href="mailto:maksimskaracuns@gmail.com?Subject=Hello!" target="_blank">Email</a>.</h2>
+                </div>
+            </div>
+        </footer>
+
+        </div>
+    </body>
+</html>
